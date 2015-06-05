c78510cdbe2b0e5c9605f2511bfe094dcf7dd2fa
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-06-05 20:01:06 +0000

    builders: add files

---
+++ mc/builders.py
@@ -0,0 +1,112 @@
+from flask import current_app
+from docker import Client
+from jinja2 import TemplateNotFound
+from mc.app import create_celery, create_jinja2
+from mc.models import db, Build
+import os
+import tarfile
+import io
+
+templates = create_jinja2()
+
+
+class Builder(object):
+    """
+    Base builder class
+    """
+    def __init__(self, commit):
+        self.commit = commit
+        self.repo = commit.repository
+        self.templates = create_jinja2()
+        self.files = []
+
+    def build(self):
+        """
+        Each class should implement their own build method
+        """
+        raise NotImplementedError
+
+
+class DockerBuilder(Builder):
+    """
+    Class responsible for finding the correct templates, rendering them,
+    creating a build context, executing docker build, and executing docker push
+    """
+
+    def get_templates(self):
+        """
+        Finds the templates using the app's create_jinja2 loader
+        """
+
+        # dockerfile
+        t = self.templates.get_template(
+            'docker/dockerfiles/{}.dockerfile.template'.format(
+                self.repo
+            )
+        )
+        self.files.append({
+            'name': 'Dockerfile',
+            'content': t.render(commit=self.commit),
+        })
+
+        # gunicorn
+        t = self.templates.get_template(
+            'docker/gunicorn/gunicorn.conf.py'
+        )
+        self.files.append({
+            'name': 'gunicorn.conf.py',
+            'content': t.render(),
+        })
+
+        t = self.templates.get_template(
+            'docker/gunicorn/gunicorn.sh'
+        )
+        self.files.append({
+            'name': 'gunicorn.sh',
+            'content': t.render(),
+        })
+
+        # cron/, etc/ iif there exists a `self.repo` directory
+        def filter(p):
+            return ("cron/" in p or "etc/" in p) and (self.repo in p) and \
+                   (not os.path.basename(p).startswith('.'))
+
+        for t in self.templates.list_templates(
+                filter_func=filter):
+
+            self.files.append({
+                'name': os.path.basename(t),
+                'content': self.templates.get_template(t).render(),
+            })
+
+    def create_docker_context(self):
+        """
+        Creates an in-memory tarfile that will be used as the docker context
+        """
+
+        self.tarfile = io.BytesIO()
+
+        with tarfile.open(fileobj=self.tarfile, mode="w|") as tar:
+            for f in self.files:
+                tarinfo = tarfile.TarInfo(f['name'])
+                tarinfo.size = len(f['content'])
+                tar.addfile(tarinfo, io.BytesIO(f['content'].encode('utf-8')))
+        self.tarfile.seek(0)  # Reset from EOF
+
+    def build(self):
+        """
+        runs docker build with the tarfile context
+        """
+        docker = Client()
+        res = docker.build(fileobj=self.tarfile, custom_context=True)
+        for i in res:
+            print i
+
+
+
+
+
+
+
+
+
---
+++ mc/tests/unittests/test_builders.py
@@ -0,0 +1,58 @@
+"""
+Test builders
+"""
+
+import sys
+import os
+PROJECT_HOME = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../../../'))
+sys.path.append(PROJECT_HOME)
+
+import unittest
+import io
+from mc.builders import DockerBuilder
+from mc.models import db, Commit
+
+
+class TestDockerBuilder(unittest.TestCase):
+    """
+    Test the DockerBuilder
+    """
+
+    def setUp(self):
+        self.commit = Commit(
+            commit_hash='master',
+            repository="adsws"
+
+        )
+        self.builder = DockerBuilder(self.commit)
+
+    def tearDown(self):
+        self.commit = None
+        self.builder = None
+
+    def test_get_templates(self):
+        """
+        Test that the builder grabs the expected templates
+        """
+        self.builder.get_templates()
+        self.assertGreater(len(self.builder.files), 0)
+        self.assertEqual(self.builder.files[0]['name'], 'Dockerfile')
+        self.assertIn(self.commit.commit_hash, self.builder.files[0]['content'])
+
+    def test_create_docker_context(self):
+        """
+        Test that the docker context is created in-memory and that it has
+        a non zero size
+        """
+        self.builder.get_templates()
+        self.builder.create_docker_context()
+        self.assertIsInstance(self.builder.tarfile, io.BytesIO)
+        self.assertGreater(len(self.builder.tarfile.readlines()), 0)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
