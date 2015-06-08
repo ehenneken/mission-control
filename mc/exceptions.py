4922e984e898d82529df85cbe1fe1d78ea16a1a0
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-06-08 18:34:22 +0000

    exceptions: add custom BuildError exc

--- mc/exceptions.py
+++ mc/exceptions.py
@@ -21,3 +21,13 @@ class InvalidSignature(Exception):
         self.value = value
     def __str__(self):
         return repr(self.value)
+
+
+class BuildError(Exception):
+    """
+    Raised when a build does not complete successfully
+    """
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
