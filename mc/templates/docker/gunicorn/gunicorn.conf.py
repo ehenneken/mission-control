c7f2653d0873ab797925749309c8404840ee9dff
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-06-12 20:09:02 +0000

    templates: constrain gunicorn work concurrency in the (5,10) range

--- mc/templates/docker/gunicorn/gunicorn.conf.py
+++ mc/templates/docker/gunicorn/gunicorn.conf.py
@@ -1,11 +1,12 @@
-import multiprocessing,os
+import multiprocessing
+import os
  
 APP_NAME = os.environ.get('SERVICE', 'generic_service')
 LOG_DIR = '/tmp'
 
 bind = "0.0.0.0:80"
 #bind = "unix:/tmp/gunicorn-{}.sock".format(APP_NAME)
-workers = min(5,multiprocessing.cpu_count() * 2 + 1)
+workers = min(10, min(5, multiprocessing.cpu_count() * 2 + 1))
 max_requests = 200
 preload_app = True
 chdir = os.path.dirname(__file__)
@@ -14,4 +15,4 @@ debug = False
 errorlog = '{}/{}.error.log'.format(LOG_DIR, APP_NAME)
 accesslog = '{}/{}.access.log'.format(LOG_DIR, APP_NAME)
 pidfile = '{}/{}.pid'.format(LOG_DIR, APP_NAME)
-loglevel="info"
+loglevel = "info"
