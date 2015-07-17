dd76226dd41b4790dd8c1e7b68532e89e71d64ac
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-07-17 18:15:37 +0000

    templates: cron requires newline at EOF

--- mc/templates/docker/cron/adsws/cronjob.sh
+++ mc/templates/docker/cron/adsws/cronjob.sh
@@ -1,3 +1,4 @@
 * 1 * * * bash -l -c 'python /adsws/manage.py accounts cleanup_tokens' >> /tmp/cron.log 2>&1
 * 2 * * * bash -l -c 'python /adsws/manage.py accounts cleanup_clients' >> /tmp/cron.log 2>&1
 * */2 * * * bash -l -c 'python /adsws/manage.py accounts cleanup_users' >> /tmp/cron.log 2>&1
+
