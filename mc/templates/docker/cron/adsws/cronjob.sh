010fbf8d76868da0101a6daf3167945a091dce30
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-07-17 14:36:01 +0000

    templates: update adsws/cron template

--- mc/templates/docker/cron/adsws/cronjob.sh
+++ mc/templates/docker/cron/adsws/cronjob.sh
@@ -1,3 +1,3 @@
-* 1 * * * source /etc/container_environment.sh && python /adsws/manage.py accounts cleanup_tokens  >> /tmp/cron.log 2>&1
-* 2 * * * source /etc/container_environment.sh && python /adsws/manage.py accounts cleanup_clients >> /tmp/cron.log 2>&1
-* */2 * * * source /etc/container_environment.sh && python /adsws/manage.py accounts cleanup_users >> /tmp/cron.log 2>&1
+* 1 * * * bash -l -c 'python /adsws/manage.py accounts cleanup_tokens' >> /tmp/cron.log 2>&1
+* 2 * * * bash -l -c 'python /adsws/manage.py accounts cleanup_clients' >> /tmp/cron.log 2>&1
+* */2 * * * bash -l -c 'python /adsws/manage.py accounts cleanup_users' >> /tmp/cron.log 2>&1
