99a9b625c4d2e4e6dccd518d4350c981a304a957
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-08-13 17:28:13 +0000

    exceptions: cleanup

--- mc/exceptions.py
+++ mc/exceptions.py
@@ -7,47 +7,27 @@ class NoSignatureInfo(Exception):
     """
     Raised when no signature info is found
     """
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
 
 class InvalidSignature(Exception):
     """
     Raised when the signature is not validated
     """
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
 
 class BuildError(Exception):
     """
     Raised when a build does not complete successfully
     """
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
 
 class UnknownRepoError(Exception):
     """
     Raised when a repo is not known to mc
     """
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
 
 class UnknownServiceError(Exception):
     """
     Raised when a service is not known to mc
     """
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
